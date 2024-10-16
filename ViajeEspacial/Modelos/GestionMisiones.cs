ae337237f7792f1ea605e6ced6b35b28c9cdf5ab
DanteO7 <166159559+DanteO7@users.noreply.github.com>
2024-10-16 17:51:49 -0300

    Ejercicio de viajes espaciales en POO

--- ViajeEspacial/Modelos/GestionMisiones.cs
+++ ViajeEspacial/Modelos/GestionMisiones.cs
@@ -1,28 +1,58 @@
-﻿namespace ViajeEspacial.Modelos
+namespace ViajeEspacial.Modelos
 {
     public static class GestionMisiones
     {
         static List<Mision> Misiones = new();
         public readonly static string ArchivoMisiones = "misiones.txt";
 
-        public static void AgregarMision()
+        public static void AgregarMision(Mision mision)
         {
-
+            Misiones.add(mision);
+            Console.WriteLine($"Mision '{mision.Nombre}' agregada");
         }
 
-        public static void EliminarMision()
+        public static void EliminarMision(string nombre)
         {
+            var mision = Misiones.Find(m => m.Name == nombre);
 
+            if (mision == null)
+            {
+                Console.WriteLine($"Mision '{nombre}' no encontrada");
+                return;
+            }
+
+            Misiones.Remove(mision);
+            Console.WriteLine($"Mision '{nombre}' eliminada");
         }
 
-        public static void ModificarMision()
+        public static void ModificarMision(string nombre, Mision nuevaMision)
         {
+            var mision = Misiones.Find(m => m.Name == nombre);
 
+            if(mision == null)
+            {
+                Console.WriteLine($"Mision '{nombre}' no encontrada");
+                return;
+            }
+
+            Misiones.Remove(mision);
+            Misiones.Add(nuevaMision);
+            Console.WriteLine($"Misión '{nombre}' modificada");
         }
 
         public static void MostrarMisiones()
         {
+            if(Misiones.Count == 0)
+            {
+                Console.WriteLine("No hay misiones");
+                return;
+            }
 
+            Console.WriteLine("Lista de misiones:");
+            foreach(var mision in Misiones)
+            {
+                Console.WriteLine(mision);
+            }
         }
 
         public static void GuardarDatos()
